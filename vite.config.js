1d745d1431090ebb6cb823e4ff8b24a955972dd1
mohdsajid9600 <mohdsajid9600@gmail.com>
2026-08-05 17:51:01 +0530

    fixed deploy issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-profile/', // <-- Add this
+  base: process.env.VERCEL ? '/' : './',
 
   plugins: [
     react(),
